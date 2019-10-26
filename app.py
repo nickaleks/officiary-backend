58a70711b5189d4930711fd7341f3f1953f4b2b4
Nikita Alekseev <n.alekseev2104@gmail.com>
2019-10-26 20:24:27 +0300

    add ratings

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import json
+import db
 
 app = Flask(__name__)
 CORS(app)
@@ -11,8 +12,15 @@ class Rating:
         self.name = name
         self.entries = entries
 
+class RatingEntry:
+    def __init__(self, person_id, person_name, rating_value):
+        self.person_id = person_id
+        self.person_name = person_name
+        self.rating_value = rating_value
+
 @app.route('/rating/all')
 def all_ratings():
+    db.declarations.find()
     return json.dumps([Rating(1, "Pidor Rating", [{"name": "pidor", "rating_value": 0.1337}, {"name": "ebaniy", "rating_value": 0.1228}]).__dict__])
 
 @app.route('/rating/<id>')
