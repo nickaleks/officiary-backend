fa36d40fa9e4f2b03b5200ee9c9be655db57d823
Nikita Alekseev <n.alekseev2104@gmail.com>
2019-10-26 18:26:21 +0300

    add rating api

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+import json
+import pymongo
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class Rating:
+    def __init__(self, id, name, entries):
+        self.id = id
+        self.name = name
+        self.entries = entries
+
+@app.route('/rating/all')
+def all_ratings():
+    return json.dumps([Rating(1, "Pidor Rating", [{"name": "pidor", "rating_value": 0.1337}, {"name": "ebaniy", "rating_value": 0.1228}]).__dict__])
+
+@app.route('/rating/<id>')
+def get_rating(id):
+    return Rating(1, "Pidor Rating", [{"name": "pidor", "rating_value": 0.1337}, {"name": "ebaniy", "rating_value": 0.1228}]).__dict__
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run()
